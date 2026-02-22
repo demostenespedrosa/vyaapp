bbac420a28b77c50e02a303a52a54e6dfe0ee28f
Demóstenes Filho <demostenestj@gmail.com>
2026-02-22 19:04:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9887886881-64576",
+  "appId": "1:270486529799:web:a3c2641456f4fd36b547a2",
+  "apiKey": "AIzaSyAQD4BaBlhGZYb_Wq-jFIJEoXATgYj0eeE",
+  "authDomain": "studio-9887886881-64576.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "270486529799"
+};
